5fbe2d5ddf36c859b7a2b7df2849943278ebc73b
HanssenGLRP <66432612+186622@users.noreply.github.com>
2024-10-29 11:28:43 +0100

    Oppgave1: a) og b)
    Implementert konstrultøre, og private objektvariabler

--- src/main/java/no/hvl/dat100/oppgave1/Innlegg.java
+++ src/main/java/no/hvl/dat100/oppgave1/Innlegg.java
@@ -4,23 +4,29 @@ import no.hvl.dat100.common.TODO;
 
 public abstract class Innlegg {
 	
-	// TODO - deklarering av objektvariable
+	private int id;
+	private String bruker;
+	private String dato;
+	private int likes;
 	
 	public Innlegg() {
 		
 	}
 	
 	public Innlegg(int id, String bruker, String dato) {
-
-		// TODO 
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+		likes = 0;
+		this.id = id;
+		this.bruker = bruker;
+		this.dato = dato;
+	
 	}
 
 	public Innlegg(int id, String bruker, String dato, int likes) {
-
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+		this.id = id;
+		this.bruker = bruker;
+		this.dato = dato;
+		this.likes = likes;
+	
 	}
 	
 	public String getBruker() {
